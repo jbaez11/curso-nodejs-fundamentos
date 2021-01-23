d80bce85e12abef3fd3c05dd95e4593fae50cbd4
Jonathan Baez <jbaez9910@gmail.com>
2021-01-23 16:05:43 -0500

    modulos globales

---
+++ modulos/globales.js
@@ -0,0 +1,21 @@
+// let i=0;
+// let intervalo = setInterval(function(){
+//     console.log('Hola');
+//     if(i === 3){
+//         clearInterval(intervalo);
+//     }
+//     i++;
+// })
+
+
+// setImmediate(function(){
+//     console.log('Hola')
+// })
+
+
+// console.log(__dirname);
+// console.log(__filename)
+
+globalThis.miVariable = 'elvalor';
+
+console.log(miVariable);
