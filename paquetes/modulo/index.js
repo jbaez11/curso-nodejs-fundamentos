01c42d2d9d5184e5ee91f2ad563f6db9392e7634
Jonathan Baez <jbaez9910@gmail.com>
2021-01-26 13:48:45 -0500

    trabajar con buffers en NodeJs

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,8 @@
+//traer modulo 
+const modulo = require('./modulo');
+
+// ejecutar una funcion del modulo
+modulo.saluda();
+console.log(modulo.prop1);
+
+
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,10 @@
+function saluda(){
+        console.log('hola');
+}
+
+module.exports = {
+    saluda,
+    prop1 : 'ho.a que tal'
+
+}
+
