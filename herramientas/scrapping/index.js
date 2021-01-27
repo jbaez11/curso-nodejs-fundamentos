e32eb75e0b8e5aa8966c17473d9f4964c5b84cca
Jonathan Baez <jbaez9910@gmail.com>
2021-01-27 18:39:10 -0500

    Herramienta de scrapping en NodeJS

---
+++ herramientas/scrapping/index.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+
+//funcion auto ejecutada
+(async() => {
+    console.log('lanzamos navegador');
+    //const browser = await puppeteer.launch();
+    
+    //poder ver el navegador
+    const browser = await puppeteer.launch({ headless: false});
+
+    const page  = await browser.newPage();
+    await page.goto('https://es.wikipedia.org/wiki/Node.js');
+
+    var titulo1 = await page.evaluate(() => {
+        const h1 = document.querySelector('h1');
+        console.log(h1.innerHTML);
+
+        return h1.innerHTML;
+    })
+
+    console.log(titulo1);
+
+    console.log('cerramos navegador');
+    browser.close();
+    console.log('navegador cerrado');
+
+})();
